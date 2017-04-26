fa78fd0ddb7764a0e313c92475e675edb284ef0e
Bruno Amaral <mail@brunoamaral.eu>
2017-04-26 21:26:28 +0100

    wip

--- scheduler.js
+++ scheduler.js
@@ -1,14 +1,17 @@
-var config = require('../config.js');
-var johnny = require('../johnnybot');
+var config = require('./config.js');
+var database = require('./database.json');
+var johnny = require('./johnnybot');
 var schedule = require('node-schedule');
 var sqlite3 = require('sqlite3').verbose();
 var SunCalc = require('suncalc');
+var Client = require('node-rest-client').Client;
+var client = new Client();
 
 var run_once = false;
 // we want the scheduler bellow to run only once
 
-var j = schedule.scheduleJob('* 17-23 * * *', function() {
-
+// var j = schedule.scheduleJob('* 17-23 * * *', function() {
+var j = schedule.scheduleJob('* * * * *', function() {
 	if (run_once === false) {
 
     	var db = new sqlite3.Database(database.prod.filename);
@@ -20,25 +23,29 @@ var j = schedule.scheduleJob('* 17-23 * * *', function() {
 
 	            } else {
 	                // There is someone home
-
-	                var url = config.philipsbridge + 'api/' + config.philipsbridge_user + '/lights';
-	                client.get(url, function (data, response) {
-	                    // parsed response body as js object 
-	                    if (data[1].state.on === true ){
-	                      // The light is on, do nothing.
-	                    }else{
-	                      // The light is off, do something
-	                      var times = SunCalc.getTimes(new Date(), config.home_town_lat, config.home_town_long);
-	                      var today = new Date();
-	                      if (today <= times['sunrise'] || today >= times['sunset']) {
+	                console.log('there is someone home')
+	                var times = SunCalc.getTimes(new Date(), config.home_town_lat, config.home_town_long);
+	                var today = new Date();
+	                if (today <= times['sunrise'] || today >= times['sunset']) {
+	                	console.log('sun is down');
+		                var url = config.philipsbridge + 'api/' + config.philipsbridge_user + '/lights';
+		                client.get(url, function (data, response) {
+		                    if (data[1].state.on === true ){
+		                    	console.log('the light is on')
+		                      // The light is on, do nothing.
+		                      run_once = true;
+		                    }else{
+		                      // The light is off, do something
+		                      console.log('the light is off')
 	                          johnny.sendMessage(config.telegram_chat_id, 'It\'s so dark! I am going to turn on the lights');
 	                          command.lights(true);
 	                          run_once = true;
+	                     
 	                      }
-	                    }
+	                    });
 
-	            }
-	        });
+	            } ;
+	        };
 	    });
 	    db.close();
 
