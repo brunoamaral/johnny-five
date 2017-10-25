5f4aa6988f6f2fa78b827726addee7f0ea3d1a66
Bruno Amaral <mail@brunoamaral.eu>
2017-10-25 09:13:02 +0100

    new command to send message

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var johnnybot = require('./johnnybot');
 var scheduler = require('./scheduler');
-
+var cors = require('cors');
 var app = express();
 
 // view engine setup
@@ -32,13 +32,20 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-
+app.use(function(req, res, next) {
+	  res.header("Access-Control-Allow-Origin", "*");
+	  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	  next();
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
+app.post('/telegram', function(req, res, next) {
+	 // Handle the post for this route
+ });
 
 // error handlers
 
